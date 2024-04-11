b562482ac98a5947e7dc7f615984012bd9a6b46f
SandunSenevirathna <sandunsandatharu@gmail.com>
2024-04-11 17:48:06 +0530

    make string, number, boolean and null data types

--- Data Types/index.js
+++ Data Types/index.js
@@ -11,3 +11,22 @@ Non-Primitive
     • Object
 
 */
+
+var firstName = "sandun",
+  age = 24,
+  gender = "Male",
+  married = false,
+  middleName = null,
+  country;
+
+if (married) {
+  console.log(
+    `this is ${firstName}, he is ${age} and ${gender}. at this moment he is married. his middle name is ${middleName}`
+  );
+} else {
+  console.log(
+    `this is ${firstName}, he is ${age} and ${gender}. at this moment he is not married. his middle name is ${middleName}`
+  );
+}
+
+console.log(typeof(country));
