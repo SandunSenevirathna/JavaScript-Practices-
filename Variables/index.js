dd9358935c1ef2461085d6a389429c7536e207cc
SandunSenevirathna <sandunsandatharu@gmail.com>
2024-04-11 14:03:35 +0530

    Create variables and add some small part

---
+++ Variables/index.js
@@ -0,0 +1,6 @@
+var oldScore = 150;
+var newScore = 100;
+
+console.log(` Old Score = ${oldScore }`);
+console.log(` New Score = ${newScore}`);
+console.log(` Total Score = ${oldScore + newScore}`);
