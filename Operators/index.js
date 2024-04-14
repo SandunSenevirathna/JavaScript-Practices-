2e6e971806c0a3e6ed407889b70f202aa439ab07
SandunSenevirathna <sandunsandatharu@gmail.com>
2024-04-14 08:26:01 +0530

    make Operators

---
+++ Operators/index.js
@@ -0,0 +1,18 @@
+var numberOne = 10;
+var numberTwo = 20;
+var firstName = 'Sandun';
+var lastName = 'Senevirathna';
+
+console.log(numberOne + numberTwo);
+console.log(numberOne - numberTwo);
+console.log(numberOne / numberTwo);
+console.log(numberOne * numberTwo);
+
+console.log(numberOne++);
+console.log(++numberOne);
+console.log(numberOne--);
+console.log(--numberOne);
+
+
+
+console.log(firstName + ' ' + lastName);
