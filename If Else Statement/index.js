b676c65df8c766cc568466fefc7f1ea471b33d41
SandunSenevirathna <sandunsandatharu@gmail.com>
2024-04-14 13:05:46 +0530

    If Else Statement

---
+++ If Else Statement/index.js
@@ -0,0 +1,11 @@
+var number = 20;
+
+if(number >= 100){
+ console.log ("this number is more than hundred");   
+}
+else{
+ console.log ("this number is less or equals than hundred");   
+
+}
+
+(number != 20) ? console.log("not equals to 20") : console.log('equals to 20');
