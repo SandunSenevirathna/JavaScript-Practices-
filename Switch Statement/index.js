85f619b727fc6871d1c02a4038d1150ea1d8ac4b
SandunSenevirathna <sandunsandatharu@gmail.com>
2024-04-17 11:00:24 +0530

    make some switch case

---
+++ Switch Statement/index.js
@@ -0,0 +1,16 @@
+var dayOfWeek = 6;
+
+switch (dayOfWeek) {
+  case 1:
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+    console.log("Week Day");
+    break;
+  case 6:
+  case 7:
+    console.log("Week End Day")
+  default:
+    console.log("Invalid Day of Week");
+}
