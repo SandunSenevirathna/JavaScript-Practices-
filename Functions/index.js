aa93e386f0e4dc84ce5b56a36cb17ed1384bb5ce
SandunSenevirathna <sandunsandatharu@gmail.com>
2024-04-18 12:04:13 +0530

    make a small functions

---
+++ Functions/index.js
@@ -0,0 +1,45 @@
+var daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+function printDate(daysOfWeek, number) {
+  switch (number) {
+    case 0:
+      document.writeln(daysOfWeek[number] + "<br>");
+      break;
+    case 1:
+      document.writeln(daysOfWeek[number] + "<br>");
+      break;
+    case 2:
+      document.writeln(daysOfWeek[number] + "<br>");
+      break;
+    case 3:
+      document.writeln(daysOfWeek[number] + "<br>");
+      break;
+    case 4:
+      document.writeln(daysOfWeek[number] + "<br>");
+      break;
+    case 5:
+      document.writeln(daysOfWeek[number] + "<br>");
+      break;
+    case 6:
+      document.writeln(daysOfWeek[number] + "<br>");
+      break;
+    default:
+      document.writeln("Invalide Input");
+  }
+}
+
+function getReturnValue(num1, num2){
+    return(num1 * num2)
+}
+
+
+printDate(daysOfWeek, 1);
+document.writeln(getReturnValue(6, 10));
